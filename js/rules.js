122a10b944fcf5128571bf60cf0af7064e8a9e2f
Scott Campbell <SLCampbell96@gmail.com>
2016-07-20 10:05:38 -0400

    Working Demo

---
+++ js/rules.js
@@ -0,0 +1,100 @@
+function highLife() {
+   // B36/S23
+   $("#l1").prop('checked', false);
+   $("#l2").prop('checked', false);
+   $("#l3").prop('checked', true);
+   $("#l4").prop('checked', false);
+   $("#l5").prop('checked', false);
+   $("#l6").prop('checked', true);
+   $("#l7").prop('checked', false);
+   $("#l8").prop('checked', false);
+   $("#d1").prop('checked', false);
+   $("#d2").prop('checked', true);
+   $("#d3").prop('checked', true);
+   $("#d4").prop('checked', false);
+   $("#d5").prop('checked', false);
+   $("#d6").prop('checked', false);
+   $("#d7").prop('checked', false);
+   $("#d8").prop('checked', false);
+   setRules();
+}
+function replicator() {
+   // B1357/S1357
+   $("#l1").prop('checked', true);
+   $("#l2").prop('checked', false);
+   $("#l3").prop('checked', true);
+   $("#l4").prop('checked', false);
+   $("#l5").prop('checked', true);
+   $("#l6").prop('checked', false);
+   $("#l7").prop('checked', true);
+   $("#l8").prop('checked', false);
+   $("#d1").prop('checked', true);
+   $("#d2").prop('checked', false);
+   $("#d3").prop('checked', true);
+   $("#d4").prop('checked', false);
+   $("#d5").prop('checked', true);
+   $("#d6").prop('checked', false);
+   $("#d7").prop('checked', true);
+   $("#d8").prop('checked', false);
+   setRules();
+}
+function dayNight() {
+   //B3678/S34678
+   $("#l1").prop('checked', false);
+   $("#l2").prop('checked', false);
+   $("#l3").prop('checked', true);
+   $("#l4").prop('checked', false);
+   $("#l5").prop('checked', false);
+   $("#l6").prop('checked', true);
+   $("#l7").prop('checked', true);
+   $("#l8").prop('checked', true);
+   $("#d1").prop('checked', false);
+   $("#d2").prop('checked', false);
+   $("#d3").prop('checked', true);
+   $("#d4").prop('checked', true);
+   $("#d5").prop('checked', false);
+   $("#d6").prop('checked', true);
+   $("#d7").prop('checked', true);
+   $("#d8").prop('checked', true);
+   setRules();
+}
+function maze() {
+   //B3/S12345
+   $("#l1").prop('checked', false);
+   $("#l2").prop('checked', false);
+   $("#l3").prop('checked', true);
+   $("#l4").prop('checked', false);
+   $("#l5").prop('checked', false);
+   $("#l6").prop('checked', false);
+   $("#l7").prop('checked', false);
+   $("#l8").prop('checked', false);
+   $("#d1").prop('checked', true);
+   $("#d2").prop('checked', true);
+   $("#d3").prop('checked', true);
+   $("#d4").prop('checked', true);
+   $("#d5").prop('checked', true);
+   $("#d6").prop('checked', false);
+   $("#d7").prop('checked', false);
+   $("#d8").prop('checked', false);
+   setRules();
+}
+function walledCities() {
+   //B45678/S2345
+   $("#l1").prop('checked', false);
+   $("#l2").prop('checked', false);
+   $("#l3").prop('checked', false);
+   $("#l4").prop('checked', true);
+   $("#l5").prop('checked', true);
+   $("#l6").prop('checked', true);
+   $("#l7").prop('checked', true);
+   $("#l8").prop('checked', true);
+   $("#d1").prop('checked', false);
+   $("#d2").prop('checked', true);
+   $("#d3").prop('checked', true);
+   $("#d4").prop('checked', true);
+   $("#d5").prop('checked', true);
+   $("#d6").prop('checked', false);
+   $("#d7").prop('checked', false);
+   $("#d8").prop('checked', false);
+   setRules();
+}
